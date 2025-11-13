f98191ee6cf5c88973ff6031ca6d6e54ac47770b
Christopher <146177013+krisnat102@users.noreply.github.com>
2025-11-13 16:05:51 +0200

    sem6

---
+++ sem6.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+#pragma region Number System
+unsigned convertToBinary(unsigned n) 
+{
+    unsigned res = 0;
+    unsigned mult = 1;
+
+    while (n != 0) 
+    {
+        if (n % 2)
+        {
+            res += mult;
+        }
+
+        mult *= 10;
+        n /= 2;
+    }
+
+    return res;
+}
+
+unsigned convertFromBinaryToDecimal(unsigned n) 
+{
+    unsigned res = 0;
+    unsigned mult = 1;
+
+    while (n != 0)
+    {
+        res += (n % 10) * mult;
+        mult *= 2;
+        n /= 10;
+    }
+
+    return res;
+}
+
+unsigned getSymbolValue(char ch)
+{
+    if (ch >= '0' && ch <= '9') 
+    {
+        return ch - '0';
+    }
+
+    if (ch >= 'A' && ch <= 'Z') 
+    {
+        return ch - 'A' + 10;
+    }
+
+    return -1;
+}
+
+unsigned getSymbolFromValue(unsigned n)
+{
+    if (n <= 9)
+    {
+        return n + '0';
+    }
+
+    if (n <= getSymbolValue('Z'))
+    {
+        return n + 'A' - 10;
+    }
+
+    return -1;
+}
+
+unsigned convertFromKToDecimal(const char num[], size_t size, unsigned k)
+{
+    unsigned res = 0;
+    unsigned mult = 0;
+
+    for (int i = size - 1; i >= 0; i--) 
+    {
+        res += getSymbolValue(num[i]) * mult;
+        mult *= k;
+    }
+
+    return res;
+}
+
+void convertFromDecimalToK(unsigned num, char res[], size_t size, unsigned k)
+{
+    while (size > 0)
+    {
+        res[--size] = getSymbolFromValue(num % k);
+        num /= k;
+    }
+}
+
+void convertFromBaseKToBaseP(const char baseKNum[], size_t kSize, unsigned k, char res[], size_t pSize, unsigned p) 
+{
+    unsigned dec = convertFromKToDecimal(baseKNum, kSize, k);
+    convertFromDecimalToK(dec, res, pSize, p);
+}
+#pragma endregion
+
+void printArr(const char arr[], size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << arr[i];
+    }
+}
+
+int main()
+{
+    const char num[] = { '1', '1', '1', '1', '1'};
+    char res[2];
+    convertFromBaseKToBaseP(num, 5, 2, res, 2, 16);
+}
